8720d372280d83e5f16b4d9f6c7e7f2cb76f2dc6
nosilent <1009739751@qq.com>
2019-09-18 10:04:27 +0800

    ddc

--- js/compile.js
+++ js/compile.js
@@ -2,12 +2,13 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-09-18 09:39:32
- * @LastEditTime: 2019-09-18 10:02:06
+ * @LastEditTime: 2019-09-18 10:04:11
  * @LastEditors: Please set LastEditors
  */
 let root = document.querySelector('.body')
 let data = fetch('/docs/react.md').then(function(res){
     console.log(res)
-    return marked(res)
+    // return marked(res)
+    return null
 })
-// root.innerHTML = data
+root.innerHTML = data
