0cdb8d9f0511268374014342126f52370f8755b3
nosilent <1009739751@qq.com>
2019-09-18 11:25:56 +0800

    'd'

--- js/compile.js
+++ js/compile.js
@@ -2,12 +2,12 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-09-18 09:39:32
- * @LastEditTime: 2019-09-18 11:22:37
+ * @LastEditTime: 2019-09-18 11:25:31
  * @LastEditors: Please set LastEditors
  */
 let root = document.querySelector('.body')
 axios.get('/docs/react.md').then(function(res){
     console.log(res)
     // return marked(res)
-    root.innerHTML = res
+    root.innerHTML = res.toString()
 })
