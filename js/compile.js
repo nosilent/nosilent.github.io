dab2f88bc6b2e1f7c57b2b1972f15442bb6627df
nosilent <1009739751@qq.com>
2019-09-18 11:59:32 +0800

    ''

--- js/compile.js
+++ js/compile.js
@@ -2,11 +2,11 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-09-18 09:39:32
- * @LastEditTime: 2019-09-18 11:58:32
+ * @LastEditTime: 2019-09-18 11:59:17
  * @LastEditors: Please set LastEditors
  */
 $.get('/docs/react.md',function(res){
     console.log(1,res)
     console.log(2,marked(res))
-    $('.container').innerHTML = marked(res)
+    $('.container').eq(0).innerHTML = marked(res)
 })
