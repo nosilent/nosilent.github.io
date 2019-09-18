29b4cf5fefa4d77f1cdc452c37ee16f3315f726a
nosilent <1009739751@qq.com>
2019-09-18 11:38:48 +0800

    ''

--- js/compile.js
+++ js/compile.js
@@ -2,12 +2,12 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-09-18 09:39:32
- * @LastEditTime: 2019-09-18 11:35:31
+ * @LastEditTime: 2019-09-18 11:38:38
  * @LastEditors: Please set LastEditors
  */
 let root = document.querySelector('.body')
 $.get('/docs/react.md',function(res){
-    console.log(res.data)
-    console.log(marked(res.data))
-    root.innerHTML = marked(res.data)
+    console.log(1,res)
+    console.log(2,marked(res))
+    root.innerHTML = marked(res)
 })
