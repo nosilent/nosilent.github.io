cbb8baaf93f309147f83bbc1bdf7f9867f5e703b
nosilent <1009739751@qq.com>
2019-09-19 17:20:54 +0800

    ''

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-09-18 16:54:51
- * @LastEditTime: 2019-09-19 16:44:04
+ * @LastEditTime: 2019-09-19 17:20:45
  * @LastEditors: Please set LastEditors
  */
 var converter = new showdown.Converter()
@@ -27,21 +27,20 @@ function clickhandler(e){
     let url = e.target.textContent
     console.log(e.target.textContent);
     //渲染对应点击导航内容的目录
-    $.get(`/api/${url}-title.md`,res=>{
+    utils.Ajax(`/api/${url}-title.md`,res=>{
         if(!res)return;
         let title = document.querySelector('.list-group')
         let data = converter.makeHtml(res)
-        console.log(data)
-        title.innerHTML = data
+        console.log('1',data)
+        let className = 'list-group-item-action list-group-item'
+        utils.addProp(data,'<a',`class=\"${className}\"`,res=>{
+            console.log('修改后',res)
+            title.innerHTML = res
+        })
+        
     })
     //渲染对应内容
-    // $.get(`/api/${url}.md`,res=>{
-    //     if(!res)return;
-    //     let content = document.querySelector('.content');
-    //     content.innerHTML = converter.makeHtml(res)
-    // })
     utils.Ajax(`/api/${url}.md`,res=>{
-        console.log(res)
         if(!res)return;
         let content = document.querySelector('.content');
         content.innerHTML = converter.makeHtml(res)
--- utils.js
+++ utils.js
@@ -2,18 +2,24 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-09-19 16:32:46
- * @LastEditTime: 2019-09-19 16:40:20
+ * @LastEditTime: 2019-09-19 17:16:07
  * @LastEditors: Please set LastEditors
  */
 var utils = {
     Ajax(url,callback){
         let xhr = new XMLHttpRequest();
         xhr.open('get',url);
-        xhr.send()
+        xhr.send();
         xhr.onreadystatechange = function(){
             if(xhr.readyState===4&&xhr.status===200){
                 callback(xhr.responseText)
             }
-        }   
+        };
+        return this;   
+    },
+    addProp(target,tag,prop,fn){
+        let regExp = new RegExp(tag,'g')
+        data = target.replace(regExp,`${tag} prop`)
+        fn(data)
     }
 }
