c84b3eb0f8197fb68588a82e8c43d8734ad9c757
nosilent <1009739751@qq.com>
2019-09-19 21:21:51 +0800

    ''

--- render.js
+++ render.js
@@ -2,63 +2,90 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-09-18 16:54:51
- * @LastEditTime: 2019-09-19 18:50:25
+ * @LastEditTime: 2019-09-19 21:20:48
  * @LastEditors: Please set LastEditors
  */
-var converter = new showdown.Converter()
-let navData = 'html,css,js,typescript,jquery,bootstrap,vue,react,flutter,dart'.split(',');
-let frame = document.createDocumentFragment();
-let nav = document.querySelector('div.navbar-nav');
-let active;
-let config;
-init()
+;
+(function () {
+  var converter = new showdown.Converter()
+  let frame = document.createDocumentFragment();
+  let nav = document.querySelector('div.navbar-nav');
+  let btn_to_top = document.querySelector('.to_top')
+  let active;
+  let config;
+  init()
 
-function init() {
+  function init() {
     //导航栏内容初始化
     utils.Ajax('config.json', res => {
-        config = JSON.parse(res);
-        config.navData.forEach(item => {
-            let a = document.createElement('a');
-            a.classList.add('nav-link');
-            a.setAttribute('href', `#api/${item}`);
-            a.textContent = item;
-            frame.appendChild(a);
-        })
-        active = frame.firstChild;
-        active.classList.add('active');
-        nav.append(frame);
+      config = JSON.parse(res);
+      config.navData.forEach(item => {
+        let a = document.createElement('a');
+        a.classList.add('nav-link');
+        a.setAttribute('href', `#api/${item}`);
+        a.textContent = item;
+        frame.appendChild(a);
+      })
+      active = frame.firstChild;
+      active.classList.add('active');
+      nav.append(frame);
     })
     //导航栏点击事件
     nav.addEventListener('click', clickhandler)
-}
+    toTop()
+  }
 
-function clickhandler(e) {
+  function clickhandler(e) {
     active.classList.remove('active');
     active = e.target;
     active.classList.add('active');
     let url = e.target.textContent
     console.log(e.target.textContent);
     render(url)
-}
+  }
 
-
-function render(url) {
+  function render(url) {
     //渲染对应点击导航内容的目录
     utils.Ajax(`/api/${url}-${config.title_end_tag}.md`, res => {
-        if (!res) return;
-        let title = document.querySelector('.list-group')
-        let data = converter.makeHtml(res)
-        let className = 'list-group-item-action list-group-item'
-        utils.addProp(data, '<a', `class=\"${className}\"`, res => {
-            utils.removeTag(res, 'p', data => {
-                title.innerHTML = data;
-            })
+      if (!res) return;
+      let title = document.querySelector('.list-group');
+      let data = converter.makeHtml(res);
+      let className = 'list-group-item-action list-group-item';
+      utils.addProp(data, '<a', `class=\"${className}\"`, res => {
+        utils.removeTag(res, 'p', data => {
+          title.innerHTML = data;
         })
+      })
     })
     //渲染对应内容
     utils.Ajax(`/api/${url}.md`, res => {
-        if (!res) return;
-        let content = document.querySelector('.content');
-        content.innerHTML = converter.makeHtml(res)
+      if (!res) return;
+      let content = document.querySelector('.content');
+      content.innerHTML = converter.makeHtml(res);
     })
-}
+  }
+  //去顶部按钮处理
+  function toTop() {
+    let clientHeight = document.documentElement.clientHeight
+    let scroll = document.documentElement.scrollTop
+    if(scroll>clientHeight/2){
+      btn_to_top.hidden = false
+    }else{
+      btn_to_top.hidden = true
+    }
+    window.addEventListener('scroll',scrollHandler)
+    function scrollHandler(){
+      if(document.documentElement.scrollTop>clientHeight){
+        btn_to_top.hidden = false;
+      }
+      if(document.documentElement.scrollTop<clientHeight/2){
+        btn_to_top.hidden = true;
+      }
+    }
+    btn_to_top.addEventListener('click',toTop_handler)
+    function toTop_handler(){
+      window.scrollTo(0,0)
+    }
+  }
+
+})()
