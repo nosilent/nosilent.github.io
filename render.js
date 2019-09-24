fd720cac43c1767a56553057cf35ec8f31d00006
nosilent <1009739751@qq.com>
2019-09-24 21:44:26 +0800

    ''

--- render.js
+++ render.js
@@ -2,7 +2,7 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-09-18 16:54:51
- * @LastEditTime: 2019-09-24 21:41:46
+ * @LastEditTime: 2019-09-24 21:44:15
  * @LastEditors: Please set LastEditors
  */
 ;
@@ -265,7 +265,7 @@
       }
     }
     renderer.heading = function (text, level, raw, slugger) {
-      let anchor = compare(level, utils.head_id_sort.slug.call(utils.head_id_sort))
+      let anchor = compare(level, utils.head_id_sort.slug.bind(utils.head_id_sort))
       return `
             <h${level} id="${anchor}">
             ${text}
