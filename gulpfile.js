b3664669d49ca1c0b331a48580ff3dca3fae68e0
nosilent <1009739751@qq.com>
2019-09-25 14:06:22 +0800

    ''

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-09-25 10:01:44
- * @LastEditTime: 2019-09-25 13:43:57
+ * @LastEditTime: 2019-09-25 14:04:43
  * @LastEditors: Please set LastEditors
  */
 const {
@@ -12,6 +12,16 @@ const {
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
+const cleanCss =require('gulp-clean-css')
+exports.css = function(){
+  return src('./css/index.css')
+  .pipe(src('./css/prism.css'))
+  .pipe(cleanCss())
+  .pipe(rename({
+    extname: '.min.css'
+  }))
+  .pipe(dest('build/'))
+}
 exports.default = function () {
   return src('./render.js')
     .pipe(src('./utils.js'))
