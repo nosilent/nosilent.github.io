221ed1286493f3be77906de6f3d5a1945072ebf4
nosilent <1009739751@qq.com>
2020-01-30 15:20:52 +0800

    ''

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname,''),
-    publicPath: '/assets/',
+    // publicPath: '/assets/',
     filename: '[name].bundle.js'
   },
   plugins:[
