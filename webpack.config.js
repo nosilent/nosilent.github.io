b7a79e847b3e3718f920d10d3f4b913bd54c9c2f
nosilent <1009739751@qq.com>
2020-01-30 17:21:40 +0800

    ''

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,33 @@
 const  path = require('path') 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const babelOption = require('./babel.config.js')
+// const CleanWebpackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin
+// const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const webpack = require('webpack')
+
 module.exports = {
-  mode: process.env.NODE_ENV,
+  // mode: process.env.NODE_ENV,
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname,''),
-    // publicPath: '/assets/',
-    filename: '[name].bundle.js'
+    path: path.resolve(__dirname,'dist'),
+    publicPath: 'dist',
+    filename: '[name].js',
+    chunkFilename: '[name].js'
   },
   plugins:[
+    // new UglifyJSPlugin(),
+    // new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
-    // new BundleAnalyzerPlugin()
+    // new BundleAnalyzerPlugin(),
+    // new webpack.DefinePlugin({
+    //   'process.env.NODE_ENV': JSON.stringify('production')
+    // })
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: 'common' // 指定公共 bundle 的名称。
+    // })
   ],
   module:{
     rules:[
@@ -44,10 +57,10 @@ module.exports = {
       minChunks: 1,
       maxAsyncRequests: 6,
       maxInitialRequests: 4,
-      automaticNameDelimiter: '~',
+      automaticNameDelimiter: '_',
       automaticNameMaxLength: 30,
       cacheGroups: {
-        defaultVendors: {
+        node_modules: {
           test: /[\\/]node_modules[\\/]/,
           priority: -10,
           reuseExistingChunk: true
